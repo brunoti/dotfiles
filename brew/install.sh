8cf227cce2cf580b407e400c52efa42e7bf8553e
bop <brunohdbass@gmail.com>
2024-10-22 17:47:12 -0300

    fix brew stuff

--- brew/install.sh
+++ brew/install.sh
@@ -19,20 +19,10 @@ install_homebrew() {
 
   # Run the Homebrew installation script
   mkdir -p "$HOMEBREW_PREFIX"
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" --prefix="$HOMEBREW_PREFIX"
-
-  # Adding Homebrew to PATH
-  echo 'eval "$('$HOMEBREW_PREFIX'/bin/brew shellenv)"' >> "$HOME/.bash_profile"
-  echo 'eval "$('$HOMEBREW_PREFIX'/bin/brew shellenv)"' >> "$HOME/.zprofile"
-
-  # Inform the user about additional steps
-  echo "=================================="
-  echo "Homebrew installation complete."
-  echo "To proceed, please add Homebrew to your shell configuration by running:"
-  echo "source $HOME/.bash_profile"
-  echo "or"
-  echo "source $HOME/.zprofile"
-  echo "=================================="
+  /bin/bash -c git clone https://github.com/Homebrew/brew "$HOMEBREW_PREFIX"
+  eval "$($HOMEBREW_PREFIX/bin/brew shellenv)"
+  brew update --force --quiet
+  sudo chmod -R go-w "$(brew --prefix)/share/zsh"
 }
 
 # Run the installation function
